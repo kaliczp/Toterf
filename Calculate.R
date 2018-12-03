be1f039eeeeff2c2de9fc8ee0a6f38555a58d19f
Péter Kalicz <kaliczp@gmail.com>
2018-12-03 13:13:58 +0100

    Hi-res calculation and plot added

--- Calculate.R
+++ Calculate.R
@@ -8,4 +8,14 @@ PisztrSurf.mat <- as.matrix(PisztrSurf)
 min(PisztrSurf.mat)
 max(PisztrSurf.mat)
 
-VolCal(PisztrSurf.mat, levels=341:343)
+vol.df <- VolCal(PisztrSurf.mat, levels=seq(340.5, 344.5, by = 0.1))
+
+plot(level ~ volume, vol.df, typ="n",
+     xlab = "Tározó térfogat [m^3]",
+     ylab = "Vízállás [m B. f.]")
+grid()
+lines(level ~ volume, vol.df, lwd = 2)
+## Gátkorona
+axis(2, at = 344.5, lab = FALSE, tck = 1, lty = "dashed")
+## Árapasztó
+axis(2, at = 342.25, lab = FALSE, tck = 1, lty = "dashed")
