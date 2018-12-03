27df18ed0b19e26a08e6de149f5f6d20bb9bfff1
Péter Kalicz <kaliczp@gmail.com>
2018-12-03 13:47:29 +0100

    Figure with additiona lines

--- Calculate.R
+++ Calculate.R
@@ -10,12 +10,19 @@ max(PisztrSurf.mat)
 
 vol.df <- VolCal(PisztrSurf.mat, levels=seq(340.5, 344.5, by = 0.1))
 
+korona <- 344.5
+arapaszto <- 342.25
 plot(level ~ volume, vol.df, typ="n",
      xlab = "Tározó térfogat [m^3]",
      ylab = "Vízállás [m B. f.]")
 grid()
-lines(level ~ volume, vol.df, lwd = 2)
 ## Gátkorona
 axis(2, at = 344.5, lab = FALSE, tck = 1, lty = "dashed")
+text(0, korona, paste("Gát koronaszint (", korona, " m)"), adj=c(0,1.2))
 ## Árapasztó
-axis(2, at = 342.25, lab = FALSE, tck = 1, lty = "dashed")
+axis(2, at = arapaszto, lab = FALSE, tck = 1, lty = "dashed")
+text(15000, arapaszto, paste("Árapasztó (", arapaszto, " m)"), adj=c(0,1.2))
+## A görbe 1 m biztonság alatt
+lines(level ~ volume, vol.df[vol.df$level <= 343.5,], lwd = 2)
+## A görbe 1 m biztonság felett
+lines(level ~ volume, vol.df[vol.df$level >= 343.5,], lwd = 2, lty = "dotted")
