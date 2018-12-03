71e60b1b6779b9310ce32be478b1d9a8767ebb63
Péter Kalicz <kaliczp@gmail.com>
2018-12-03 12:58:33 +0100

    Updated to new larger raster

--- Calculate.R
+++ Calculate.R
@@ -1,11 +1,11 @@
 library(rgrass7)
 
 ## Raszter beolvasás
-PisztrSurf <- readRAST("surfvec1m")
+PisztrSurf <- readRAST("surfvec1m2")
 
 ## Mátrixra konvertált
 PisztrSurf.mat <- as.matrix(PisztrSurf)
 min(PisztrSurf.mat)
 max(PisztrSurf.mat)
 
-VolCal(PisztrSurf.mat, levels=341:342)
+VolCal(PisztrSurf.mat, levels=341:343)
