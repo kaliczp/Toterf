edeffe418d52f3af154af70b8cccb09d2f7586a6
Péter Kalicz <kaliczp@gmail.com>
2018-12-08 20:04:22 +0100

    RST surface readed and plot produced

--- Calculate.R
+++ Calculate.R
@@ -3,6 +3,7 @@ library(rgrass7)
 ## Raszter beolvasás
 ## IDW beolvasás
 PisztrSurf <- readRAST("surfvec1m2")
+PisztrSurf <- readRAST("rstsurf")
 
 ## Mátrixra konvertált
 PisztrSurf.mat <- as.matrix(PisztrSurf)
@@ -10,12 +11,13 @@ min(PisztrSurf.mat)
 max(PisztrSurf.mat)
 
 vol.df <- VolCal(PisztrSurf.mat, levels=seq(340.5, 344.5, by = 0.1))
+vol.df[,2] <- round(vol.df[,2])
+write.table(vol.df, "PisztrangosTerf.csv", sep=";", dec = ",", row.names=FALSE)
 
 ## IDW
-idwvol.df <- vol.df
+# idwvol.df <- vol.df
 
 ## Plot
-
 korona <- 344.5
 arapaszto <- 342.25
 plot(level ~ volume, vol.df, typ="n",
@@ -32,6 +34,10 @@ text(15000, arapaszto, paste("Árapasztó (", arapaszto, " m)"), adj=c(0,1.2))
 lines(level ~ volume, vol.df[vol.df$level <= 343.5,], lwd = 2)
 ## A görbe 1 m biztonság felett
 lines(level ~ volume, vol.df[vol.df$level >= 343.5,], lwd = 2, lty = "dotted")
+
+## IDW line compare
+lines(level ~ volume, idwvol.df, col=2)
+
 
 ## ggplot2 kezdemény
 ggplot(vol.df, aes(volume, level)) + geom_line() + geom_point() + theme_bw()
