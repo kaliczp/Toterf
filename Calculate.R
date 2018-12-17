82223f36bfcdb20f8cfc4a60bab5754e4238cf16
Péter Kalicz <kaliczp@gmail.com>
2018-12-17 14:14:54 +0100

    Calculated and illustrated surface on the graph

--- Calculate.R
+++ Calculate.R
@@ -11,8 +11,9 @@ PisztrSurf.mat <- as.matrix(PisztrSurf)
 min(PisztrSurf.mat)
 max(PisztrSurf.mat)
 
-vol.df <- VolCal(PisztrSurf.mat, levels=seq(339, 344.5, by = 0.1))
+vol.df <- VolCal(PisztrSurf.mat, levels=seq(339, 344.5, by = 0.1), surface = TRUE)
 vol.df[,2] <- round(vol.df[,2])
+vol.df[1, "surface"] <- 0 
 write.table(vol.df, "PisztrangosTerf.csv", sep=";", dec = ",", row.names=FALSE)
 ## Ha kell beolvasás
 ## vol.df <- read.csv2("PisztrangosTerf.csv")
@@ -27,7 +28,8 @@ png("PisztrVol.png", width=4*480, height=2*480, res=2*72)
 korona <- 344.5
 arapaszto <- 341.3
 plot(level ~ volume, vol.df, typ="n",
-     xlab = expression("Tározó térfogat [ "* m^3 * " ]"),
+     xlim = c(0,41000),
+     xlab = expression("Tározótérfogat [ "* m^3 * " ]"),
      ylab = "Vízállás [m B. f.]")
 grid()
 ## Gátkorona
@@ -40,6 +42,16 @@ text(15000, arapaszto, paste("Árapasztó (", arapaszto, " m)"), adj=c(0,1.2))
 lines(level ~ volume, vol.df[vol.df$level <= 343.5,], lwd = 2)
 ## A görbe 1 m biztonság felett
 lines(level ~ volume, vol.df[vol.df$level >= 343.5,], lwd = 2, lty = "dotted")
+## Felület
+par(new = TRUE)
+plot(level ~ surface, vol.df, type="n", axes = FALSE, xlab="", ylab="",
+     xlim = c(0, 20500)
+     )
+lines(level ~ surface, vol.df, lwd =2, lty = "dashed")
+axis(3)
+mtext( expression("Vízfelület [ "* m^2 * " ]"), line = 2.5)
+legend("bottomright",c("Tározótérfogat", "Vízfelület"), lwd=2, lty=c("solid", "dashed"), bg = "white")
+par(new = FALSE)
 
 dev.off()
 
