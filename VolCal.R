961cf7175a8b3a9fa079fd8993425ac0c014a5d9
Péter Kalicz <kaliczp@gmail.com>
2018-12-17 13:58:36 +0100

    VolCal function enhanced with optional surface calculation

--- VolCal.R
+++ VolCal.R
@@ -1,10 +1,17 @@
-VolCal <- function(x, levels) {
+VolCal <- function(x, levels, surface = FALSE) {
     LevVol.df <- data.frame(level = levels, volume = NA)
+
+    if(surface) {
+        LevVol.df$surface <- NA
+    }
 
     for(ttlev in 1:nrow(LevVol.df)) {
         akt.mat <- LevVol.df[ttlev, "level"] - x
         akt.mat[akt.mat < 0] <- NA
         LevVol.df[ttlev, "volume"] <- sum(akt.mat, na.rm = TRUE)
+        if(surface) {
+            LevVol.df[ttlev, "surface"] <- sum(!is.na(akt.mat))
+        }
     }
     
     LevVol.df
